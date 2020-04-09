78b1fd9066052b8bf443fe803c1ece8549baab94
Mohamed Abubucker <abubucker.sed@gmail.com>
2020-04-09 21:55:09 +0530

    update: issue in trim() whitespace fixed now

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const VideoGrant = AccessToken.VideoGrant;
 require('dotenv').config();
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
-const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
-const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID ? process.env.TWILIO_ACCOUNT_SID.trim() : "";
+const twilioApiKeySID = process.env.TWILIO_API_KEY_SID ? process.env.TWILIO_API_KEY_SID.trim() : "";
+const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET ? process.env.TWILIO_API_KEY_SECRET.trim() : "";
 
 app.use(express.static(path.join(__dirname, 'build')));
 
